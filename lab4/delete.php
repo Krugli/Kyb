410d3a63712cfdf7b7172f9d5f52314f83570a82
Krugli <75218175+Krugli@users.noreply.github.com>
2020-11-30 10:42:44 +0300

    Add files via upload

---
+++ lab4/delete.php
@@ -0,0 +1,13 @@
+<?php
+    $mysqli = new mysqli("eu-cdbr-west-03.cleardb.net", "b830edd17e2659", "4e8638b1", "heroku_c54231ad1b217de");
+    if ($mysqli->connect_errno) {
+        echo "Не удалось подключиться к БД";
+    }
+
+    $id = $_GET['id'];
+
+    $result = $mysqli->query("DELETE FROM films WHERE id='$id'");
+
+    header("Location: index.php");
+    exit;
+?>
