55502622bbbf4fbb0c9f2e4aa58b7c2b2802b4d9
Krugli <75218175+Krugli@users.noreply.github.com>
2021-01-22 19:48:51 +0300

    Update laba363.php

--- LR3/laba363.php
+++ LR3/laba363.php
@@ -1,28 +1,23 @@
 <HTML>
-<BODY>
-<H1> Задание 6: Подсчитать число различных гласных, входящих в данный текст. </H1>
-  <FORM action="<?php print $PHP_SELF ?>" method="post">
-  
-    <p>Изначальное предложение: <INPUT type="text" name="predl1" maxlength="50"></p>
-    <p><INPUT type="submit" name="check" value="Проверить"></p>
-    </FORM>
-    <?php
-      if (isset($_POST["check"])){
-        $predl1 = trim($_POST["predl1"]);
+    <BODY>
+        <H1> Вариант 16 </H1>
+        <FORM action="<?php print $PHP_SELF ?>" method="post">
+            <p>Предложение: <INPUT type="text" name="sentence" maxlength="40"></p>
+            <p><INPUT type="submit" name="count" value="Перемешать"></p>
+        </FORM>
 
-        $zvuki = array("А", "а", "Е", "е", "Ё", "ё", "И", "и", "О", "о", "У", "у", "Ы", "ы", "Э", "э", "Ю", "ю", "Я", "я");
-        $offset = 0;
-        $counter = 0;
-        
-        for ($i=0;$i<=count($zvuki);$i++) {
-          while (($offset = strpos($predl1, $zvuki[$i], $offset))!== false) {
-            $offset = $offset + strlen($zvuki[$i]);
-            $counter++;
-          }
-        }
-        echo ("<p>". "Количество гласных в введенном тексте: $counter");
-      }
-  ?>
+        <?php
+            if (isset($_POST["count"])){
+                $sentence = trim($_POST["sentence"]);
 
-<HTML>
-<BODY>
+                $words = explode(" ", $sentence);
+
+                echo "Новое предложение: ";
+
+                for ($c = count($words)-1; $c >= 0; $c--){
+                    echo $words[$c].' ';
+                }
+            }
+        ?>
+    </BODY>
+</HTML
