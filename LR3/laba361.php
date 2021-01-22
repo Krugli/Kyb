e890bac9b2f6754d6290a0f5c0289fd842febb20
Krugli <75218175+Krugli@users.noreply.github.com>
2021-01-22 18:13:31 +0300

    Update laba361.php

--- LR3/laba361.php
+++ LR3/laba361.php
@@ -1,36 +1,36 @@
 <HTML>
-<BODY>
-  <H1> Задание 2: Проверить, можно ли из букв, входящих в слово А, составить слово В. </H1>
-    <FORM action="<?php print $PHP_SELF ?>" method="post">
-    <p>Исходное слово: <INPUT type="text" name="slovo1" maxlength="50"></p>
-    <p>Требуемое слово: <INPUT type="text" name="slovo2" maxlength="50"></p>
-    <p><INPUT type="submit" name="check" value="Проверить"></p>
-    </FORM>
+    <BODY>
+        <H1> Вариант 7 </H1>
+        <FORM action="<?php print $PHP_SELF ?>" method="post">
+            <p>Предложение: <INPUT type="text" name="sentence" maxlength="40"></p>
+            <p>Количество букв: <INPUT type="text" name="size" maxlength="40"></p>
+            <p><INPUT type="submit" name="find" value="Найти"></p>
+        </FORM>
 
-<?php
-    if (isset($_POST["check"])){
-    $slovo1 = trim($_POST["slovo1"]);
-    $slovo2 = trim($_POST["slovo2"]);
+        <?php
+            if (isset($_POST["find"])){
+                $sentence = trim($_POST["sentence"]);
+                $size = trim($_POST["size"]);
 
-    $chars1 = str_split($slovo1);
-    $len = strlen($slovo2);
+                $words = explode(" ", $sentence);
 
-    $passed = 0;
+                $f = false;
+                foreach ($words as $word){
+                    if ($size == strlen($word)){
+                        $f = true;
+                        break;
+                    }
+                }
 
-    for ($c = 0; $c < count($chars1); $c++){
-    if (stripos($slovo2,$chars1[$c]) === false){
-    break;
-           }
-           $passed++;
-           }
+                echo "Результат: ";
 
-        if ($passed >= $len){
-        echo "Можно составить";
-         }
-         else{
-          echo "Нельзя составить";
-           }
-         }
- ?>
-</BODY>
+                if ($f){
+                    echo $word;
+                }
+                else{
+                    echo "Нет такого слова";
+                }
+            }
+        ?>
+    </BODY>
 </HTML>
