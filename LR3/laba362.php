5e853a4fdcf59e6b11a3e88fecc0cea671acbba2
Krugli <75218175+Krugli@users.noreply.github.com>
2021-01-22 18:20:31 +0300

    Update laba362.php

--- LR3/laba362.php
+++ LR3/laba362.php
@@ -1,24 +1,36 @@
 <HTML>
-<BODY>
-<H1> Задание 4: Пользователем задается произвольный текст и два символа. В тексте заменить все вхождения первого символа на второй. </H1>
-  <FORM action="<?php print $PHP_SELF ?>" method="post">
-  
-    <p>Изначальное предложение: <INPUT type="text" name="predl1" maxlength="50"></p>
-    <p>Выбранный символ:<INPUT type="text" name="symvol1" maxlength="2"></p>
-    <p>Желаемый символ:<INPUT type="text" name="symvol2" maxlength="2"></p>
-    <p><INPUT type="submit" name="check" value="Заменить"></p>
-    </FORM>
-    <?php
-      if (isset($_POST["check"])){
-        $predl1 = trim($_POST["predl1"]);
-        $symvol1 = trim($_POST["symvol1"]);
-        $symvol2 = trim($_POST["symvol2"]);
-        $predl1 = str_replace($symvol1, $symvol2, $predl1);
+    <BODY>
+        <H1> Вариант 2 </H1>
+        <FORM action="<?php print $PHP_SELF ?>" method="post">
+            <p>Слово 1: <INPUT type="text" name="word1" maxlength="40"></p>
+            <p>Слово 2: <INPUT type="text" name="word2" maxlength="40"></p>
+            <p><INPUT type="submit" name="check" value="Проверить"></p>
+        </FORM>
 
-        
-        echo ("<p>". "Итоговое предложение: $predl1");
-      }
-  ?>
+        <?php
+            if (isset($_POST["check"])){
+                $word1 = trim($_POST["word1"]);
+                $word2 = trim($_POST["word2"]);
 
-<HTML>
-<BODY>
+                $chars1 = str_split($word1);
+                $len = count(str_split($word2));
+
+                $passed = 0;
+
+                for ($c = 0; $c < count($chars1); $c++){
+                    if (stripos($word2,$chars1[$c]) === false){
+                        continue;
+                    }
+                    $passed++;
+                }
+
+                if ($passed >= $len){
+                    echo "есть";
+                }
+                else{
+                    echo "нет";
+                }
+            }
+        ?>
+    </BODY>
+</HTML>
