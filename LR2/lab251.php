c4bacd4288533c0ce49aff31974cbddd505150dc
Krugli <75218175+Krugli@users.noreply.github.com>
2021-01-26 20:40:17 +0300

    Update lab251.php

--- LR2/lab251.php
+++ LR2/lab251.php
@@ -1,15 +1,22 @@
-<p> Вариант 1
+<p> Вариант 7
 <p> 
 <?php
     function F($u, $t){
-        if ($u > 0 && $t > 0){
-            return $u * $u + $t;
-        }
-        else if($u <= 0 && $t <= 0){
-            return $u + $t * $t;
+        if ($u >= 0){
+            if ($t >= 0){
+                return ($u + $t) / $u * $t;
+            }
+            else{
+                return $u + 2 * $t;
+            }
         }
         else{
-            return $u + $t;
+            if ($t >= 0){
+                return $u * $u + $t / $u;
+            }
+            else{
+                return ($u + $t * $t) / (pow($u, 3) * pow($t, 4));
+            }
         }
     }
 
@@ -19,7 +26,7 @@
     echo "<p> Аргумент A = ".$a;
     echo "<p> Аргумент B = ".$b;
 
-    $z = F($a - $b, $b * $b - $a) + F($a * $a * $b, $b * $b);
+    $z = F($a/$b, (pow($b,8)-pow($a,7))/($b*$a)) + F((pow($a,10)-pow($b,12))/($b*$b*$a-$a), $b);
 
     echo "<p> Результат = ".$z;
 ?>
