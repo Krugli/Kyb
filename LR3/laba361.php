03f9e829f396fa19f7a7cd30c9b4b5e80becd1d9
Krugli <75218175+Krugli@users.noreply.github.com>
2021-04-06 21:11:59 +0500

    Update laba361.php

--- LR3/laba361.php
+++ LR3/laba361.php
@@ -1,36 +1,37 @@
 <HTML>
-    <BODY>
-        <H1> Вариант 7 </H1>
-        <FORM action="<?php print $PHP_SELF ?>" method="post">
-            <p>Предложение: <INPUT type="text" name="sentence" maxlength="40"></p>
-            <p>Количество букв: <INPUT type="text" name="size" maxlength="40"></p>
-            <p><INPUT type="submit" name="find" value="Найти"></p>
-        </FORM>
+<BODY>
+<H1> Вариант 7 </H1>
+<FORM action="<?php print $PHP_SELF ?>" method="post">
+<p>Предложение: <INPUT type="text" name="sentence" maxlength="40"></p>
+<p>Количество букв: <INPUT type="text" name="size" maxlength="40"></p>
+<p><INPUT type="submit" name="find" value="Найти"></p>
+</FORM>
 
-        <?php
-            if (isset($_POST["find"])){
-                $sentence = trim($_POST["sentence"]);
-                $size = trim($_POST["size"]);
+<?php
+if (isset($_POST["find"])){
+$sentence = trim($_POST["sentence"]);
+$size = trim($_POST["size"]);
 
-                $words = explode(" ", $sentence);
+$words = explode(" ", $sentence);
 
-                $f = false;
-                foreach ($words as $word){
-                    if ($size == strlen($word)){
-                        $f = true;
-                        break;
-                    }
-                }
+$f = false;
+foreach ($words as $word){
+if ($size == strlen($word)/2){
+$f = true;
+break;
+}
+}
 
-                echo "Результат: ";
+echo "Результат: ";
 
-                if ($f){
-                    echo $word;
-                }
-                else{
-                    echo "Нет такого слова";
-                }
-            }
-        ?>
-    </BODY>
+if ($f){
+echo $word;
+}
+else{
+echo "Нет такого слова";
+}
+}
+?>
+</BODY>
+</HTML>
 </HTML>
