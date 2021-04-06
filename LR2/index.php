d8ccfff8e4332cabf1e00de7b9b520024d718b50
Krugli <75218175+Krugli@users.noreply.github.com>
2021-04-06 17:35:07 +0500

    Update index.php

--- LR2/index.php
+++ LR2/index.php
@@ -5,10 +5,10 @@
 	echo($d);
 ?>
 <ul id="nav">
-	<li><a href="lab241.php">Упражнение 4 (В1)</a>
-	<li><a href="lab242.php">Упражнение 4 (В8)</a>
-	<li><a href="lab251.php">Упражнение 5 (В1)</a>
-	<li><a href="lab252.php">Упражнение 5 (В8)</a>
-	<li><a href="lab261.php">Упражнение 6 (В1)</a>
-	<li><a href="lab262fail.php">Упражнение 6 (В8)</a>
+	<li><a href="lab241.php">Упражнение 4 (В7)</a>
+	<li><a href="lab242.php">Упражнение 4 (В16)</a>
+	<li><a href="lab251.php">Упражнение 5 (В7)</a>
+	<li><a href="lab252.php">Упражнение 5 (В16)</a>
+	<li><a href="lab261.php">Упражнение 6 (В7)</a>
+	<li><a href="lab262fail.php">Упражнение 6 (В16)</a>
 </ul>
