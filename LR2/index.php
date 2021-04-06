ec7d883c12476612142ed84c4db24d93a41ed355
Krugli <75218175+Krugli@users.noreply.github.com>
2021-04-06 18:57:23 +0500

    Update index.php

--- LR2/index.php
+++ LR2/index.php
@@ -10,5 +10,5 @@
 	<li><a href="lab251.php">Упражнение 5 (В7)</a>
 	<li><a href="lab252.php">Упражнение 5 (В16)</a>
 	<li><a href="lab261.php">Упражнение 6 (В7)</a>
-	<li><a href="lab262fail.php">Упражнение 6 (В16)</a>
+	<li><a href="lab262.php">Упражнение 6 (В16)</a>
 </ul>
